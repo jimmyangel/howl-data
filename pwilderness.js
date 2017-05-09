89dec9e89899228b2aca481b3a7d715de05c0f23
Ricardo Morin <morinricardo@gmail.com>
2017-05-08 19:21:44 -0700

    Some work on proposed wilderness

---
+++ pwilderness.js
@@ -0,0 +1,36 @@
+var turf = require('@turf/turf');
+
+var pW = require('./pwilderness/rdls21_albers_OR.json');
+
+var combAll = {};
+var comb = {};
+
+pW.features.forEach(function(feature) {
+  var name = feature.properties.AREA_NAMES.replace(/[^a-zA-Z0-9\(\)'-]/g, ' ');
+  var uname = feature.properties.FIRST_FIRS + name.trim();
+  if (combAll[uname]) {
+    combAll[uname].features.push(feature);
+  } else {
+    combAll[uname] = {
+      type: 'FeatureCollection',
+      name: name,
+      features: [feature]
+    };
+  }
+
+  //console.log(feature.geometry.type, feature.properties.AREA_NAMES, feature.properties.Acres, turf.area(feature)/4046.86);
+});
+
+var keys = [];
+for (var key in combAll) {
+  keys.push(key);
+}
+keys.sort();
+
+keys.forEach(function(k) {
+  console.log(combAll[k].name, combAll[k].features.length);
+});
+
+console.log(Object.keys(combAll).length);
+
+//console.log(JSON.stringify(combAll, null, 2));
